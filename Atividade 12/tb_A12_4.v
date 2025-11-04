871005308d8c757959bdc8be0025568b22cfd868
barbaramrr <86504723+barbaramrr@users.noreply.github.com>
2025-11-03 22:24:25 -0300

    Update tb_A12_4.v

--- Atividade 12/tb_A12_4.v
+++ Atividade 12/tb_A12_4.v
@@ -5,20 +5,20 @@ module tb_A12_4;
 reg clk, rst;
 wire out;
 
-A12_4 U2 (.clk(clk),.rst(rst),
+A12_4 #(.CONST(10), .WIDTH (8)) U2 (.clk(clk),.rst(rst),
          .out(out)
 );
 
 
 
-always #2 clk = ~clk;
+always #5 clk = ~clk;
 
 initial begin
     
-    clk = 1'b0;
-    rst = 1'b1; 
-    #600; 
-    rst = 0;
+    clk <= 0;
+    rst <= 1'b1; 
+    #60; 
+    rst <= 0;
     #30; 
     #200;
     $finish;
